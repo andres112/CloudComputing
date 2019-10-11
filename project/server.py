0d62beb7e57ad357274d76a6b718d2388e2a4b1e
Andres Dorado <andres.dorado90@gmail.com>
2019-10-11 15:34:56 +0200

    HTTP Codes definition for responses to each HTTP request

--- project/server.py
+++ project/server.py
@@ -9,21 +9,21 @@ app = Flask(__name__)
 CORS(app)
 
 # Set up the ENV variables
-# app.config['HOST'] = os.getenv('DB_HOST')
-# app.config['PORT'] = os.getenv('DB_PORT')
-# app.config['DBNAME'] = os.getenv('DB_DBNAME')
-# app.config['USER'] = os.getenv('DB_USER')
-# app.config['PASS'] = os.getenv('DB_PASS')
+app.config['HOST'] = os.getenv('DB_HOST')
+app.config['PORT'] = os.getenv('DB_PORT')
+app.config['DBNAME'] = os.getenv('DB_DBNAME')
+app.config['USER'] = os.getenv('DB_USER')
+app.config['PASS'] = os.getenv('DB_PASS')
 
-# app.config['HTTP_USER'] = os.getenv('HTTP_USER')
-# app.config['HTTP_PASS'] = os.getenv('HTTP_PASS')
+app.config['HTTP_USER'] = os.getenv('HTTP_USER')
+app.config['HTTP_PASS'] = os.getenv('HTTP_PASS')
 
-app.config['HOST'] = "localhost"
-app.config['DBNAME'] = "watches"
-app.config['USER'] = "watches"
-app.config['PASS'] = "watches"
-app.config['HTTP_USER'] = "cloud"
-app.config['HTTP_PASS'] = "computing"
+# app.config['HOST'] = "localhost"
+# app.config['DBNAME'] = "watches"
+# app.config['USER'] = "watches"
+# app.config['PASS'] = "watches"
+# app.config['HTTP_USER'] = "cloud"
+# app.config['HTTP_PASS'] = "computing"
 
 
 # Connection to MySQL data base
@@ -91,14 +91,14 @@ def create():
                     'sku': request.json["sku"] if validateData("sku") else None,
                     'type': request.json["type"] if validateData("type") else "watch",
                     'status': request.json["status"] if validateData("status") else "current",
-                    'gender': request.json["gender"] if validateData("gender") else "man",
-                    'year': request.json["year"],
-                    'dial_material': request.json["dial_material"],
-                    'dial_color': request.json["dial_color"],
-                    'case_material': request.json["case_material"],
-                    'case_form': request.json["case_form"],
-                    'bracelet_material': request.json["bracelet_material"],
-                    'movement': request.json["movement"]}
+                    'gender': request.json["gender"] if validateData("gender") else "None",
+                    'year': request.json["year"] if validateData("year") else "0",
+                    'dial_material': request.json["dial_material"] if validateData("dial_material") else "",
+                    'dial_color': request.json["dial_color"] if validateData("dial_color") else "",
+                    'case_material': request.json["case_material"] if validateData("case_material") else "",
+                    'case_form': request.json["case_form"] if validateData("case_form") else "",
+                    'bracelet_material': request.json["bracelet_material"] if validateData("bracelet_material") else "",
+                    'movement': request.json["movement"] if validateData("movement") else ""}
 
                 sql = ("INSERT INTO watches"
                        " (sku, type, status, gender, year, dial_material, dial_color,"
@@ -116,7 +116,7 @@ def create():
         return make_response(jsonify(result), 400)
     finally:
         connection.close()
-    return make_response(jsonify(result), 201)
+    return make_response(jsonify(result), 200)
 
 # GET, PUT, DELETE: /watch/{sku}
 @app.route('/info/v1/watch/<sku>', methods=['GET', 'DELETE', 'PUT'])
@@ -131,21 +131,21 @@ def skuOperations(sku):
             cursor.execute(skuQuestion, (sku,))
             result = cursor.fetchone()
 
-            # Update operation rely on the sku id
-            if request.method == "PUT":
-                watch = {
-                    'type': request.json["type"] if validateData("type") else result["type"],
-                    'status': request.json["status"] if validateData("status") else result["status"],
-                    'gender': request.json["gender"] if validateData("gender") else result["gender"],
-                    'year': request.json["year"] if validateData("year") else result["year"],
-                    'dial_material': request.json["dial_material"] if validateData("dial_material") else result["dial_material"],
-                    'dial_color': request.json["dial_color"] if validateData("dial_color") else result["dial_color"],
-                    'case_material': request.json["case_material"] if validateData("case_material") else result["case_material"],
-                    'case_form': request.json["case_form"] if validateData("case_form") else result["case_form"],
-                    'bracelet_material': request.json["bracelet_material"] if validateData("bracelet_material") else result["bracelet_material"],
-                    'movement': request.json["movement"] if validateData("movement") else result["movement"]}
+            if(result):
+                # Update operation rely on the sku id
+                if request.method == "PUT":
+                    watch = {
+                        'type': request.json["type"] if validateData("type") else result["type"],
+                        'status': request.json["status"] if validateData("status") else result["status"],
+                        'gender': request.json["gender"] if validateData("gender") else result["gender"],
+                        'year': request.json["year"] if validateData("year") else result["year"],
+                        'dial_material': request.json["dial_material"] if validateData("dial_material") else result["dial_material"],
+                        'dial_color': request.json["dial_color"] if validateData("dial_color") else result["dial_color"],
+                        'case_material': request.json["case_material"] if validateData("case_material") else result["case_material"],
+                        'case_form': request.json["case_form"] if validateData("case_form") else result["case_form"],
+                        'bracelet_material': request.json["bracelet_material"] if validateData("bracelet_material") else result["bracelet_material"],
+                        'movement': request.json["movement"] if validateData("movement") else result["movement"]}
 
-                if(result):
                     sql = ("UPDATE watches"
                            " SET type=%s, status=%s, gender=%s, year=%s, dial_material=%s,"
                            " dial_color=%s, case_material=%s, case_form=%s, bracelet_material=%s, movement=%s"
@@ -156,27 +156,28 @@ def skuOperations(sku):
                     connection.commit()
                     result = {
                         "message": "Register {} updated successfuly".format(sku)}
-                else:
-                    result = {
-                        "message": "Register {} was not found in database".format(sku)}
 
-            # Delete operation rely on the sku id
-            if request.method == "DELETE":
-                if(result):
+                # Delete operation rely on the sku id
+                if request.method == "DELETE":
                     sql = "DELETE FROM `watches` WHERE `sku`=%s"
                     cursor.execute(sql, (sku,))
                     connection.commit()
                     result = {
                         "message": "Register {} deleted successfuly".format(sku)}
-                else:
-                    result = {
-                        "message": "Register {} was not found in database".format(sku)}
+
+                return make_response(jsonify(result), 200)
+
+            else:
+                result = {
+                    "error": "Thera are not registers for {}".format(sku)}
+                return make_response(jsonify(result), 404)
+
     except pymysql.MySQLError as e:
         result = {"error": str(e)}
         return make_response(jsonify(result), 400)
     finally:
         connection.close()
-    return make_response(jsonify(result), 200)
+
 
 # GET: /watch/complete-sku{prefix}
 @app.route('/info/v1/watch/complete-sku/<prefix>', methods=['GET'])
@@ -185,17 +186,26 @@ def getByPrefix(prefix):
     connection = dbConnection()
     try:
         with connection.cursor() as cursor:
-            # Get data base on sku prefix id by default
+            # Get data based on sku prefix id
             prefixQuestion = "SELECT * FROM `watches` WHERE `sku` LIKE \'{}\' ".format(
                 prefix+'%')
             cursor.execute(prefixQuestion)
             result = cursor.fetchall()
+
+            # Response validation
+            if result:
+                return make_response(jsonify(result), 200)
+            else:
+                result = {
+                    "error": "There are not registers for {} ".format(prefix)}
+                return make_response(jsonify(result), 404)
+
     except pymysql.MySQLError as e:
         result = {"error": str(e)}
         return make_response(jsonify(result), 400)
     finally:
         connection.close()
-    return make_response(jsonify(result), 200)
+    
 
 # GET: /watch/find
 @app.route('/info/v1/watch/find', methods=['GET'])
@@ -206,19 +216,20 @@ def getByParameters():
     request_Params = ""
     try:
         with connection.cursor() as cursor:
-
+            # get data from URL sring params
             sku = request.args.get('sku')
             watch_type = request.args.get('type')
             status = request.args.get('status')
             gender = request.args.get('gender')
             year = request.args.get('year')
 
+            # Validate the string params
             parameters = {"sku": sku, "type": watch_type,
                           "status": status, "gender": gender, "year": year}
             for key, value in parameters.items():
                 if value != None:
                     if len(request_Params) > 0:
-                        request_Params = request_Params+ " AND"
+                        request_Params = request_Params + " AND"
                     if key == "sku":
                         request_Params = request_Params + \
                             ' {} LIKE \'{}\''.format(key, value+'%')
@@ -226,16 +237,25 @@ def getByParameters():
                         request_Params = request_Params + \
                             ' {} = \'{}\''.format(key, value)
 
+            # SQL question, It only include the params different of None
             prefixQuestion = "SELECT * FROM `watches` WHERE {}".format(
                 request_Params)
             cursor.execute(prefixQuestion)
             result = cursor.fetchall()
+
+            # Response validation
+            if result:
+                return make_response(jsonify(result), 200)
+            else:
+                result = {
+                    "error": "There are not registers according to the options"}
+                return make_response(jsonify(result), 404)
+
     except pymysql.MySQLError as e:
         result = {"error": str(e)}
         return make_response(jsonify(result), 400)
     finally:
         connection.close()
-    return make_response(jsonify(result), 200)
 
 
 if __name__ == "__main__":
